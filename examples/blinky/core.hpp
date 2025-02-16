6b686590c2631348c884ccfa8a0f4e4c7e4ecd31
Antony Chan <antony.sigma@gmail.com>
2025-02-16 15:21:37 -0800

    Clock config for LGT8328P

--- examples/blinky/core.hpp
+++ examples/blinky/core.hpp
@@ -24,9 +24,9 @@ template <uint16_t factor> constexpr uint8_t prescalerRegisterValue() {
     }
 }
 
+constexpr uint32_t clock_frequency_kHz = 16'000;
 template <uint16_t prescaler_value, uint32_t time_interval_ms>
 constexpr uint8_t overflowRegisterValue() {
-    constexpr uint32_t clock_frequency_kHz = 16'000;
     constexpr uint32_t overflow_value =
         clock_frequency_kHz * time_interval_ms / prescaler_value - 1;
     static_assert(
@@ -36,7 +36,28 @@ constexpr uint8_t overflowRegisterValue() {
 }
 static_assert(overflowRegisterValue<64, 1>() == 0xF9);
 
+template <uint32_t clock_frequency_kHz> constexpr uint8_t clockPrescaler() {
+    constexpr auto oscillator_freq_kHz = 32'000UL;
+    constexpr auto division = oscillator_freq_kHz / clock_frequency_kHz;
+
+    static_assert(division <= 256);
+    static_assert(__builtin_popcount(division) == 1, "Must be power of two");
+    return std::log2(division);
+}
+static_assert(clockPrescaler<16'000UL>() == 0x01);
+static_assert(clockPrescaler<8'000UL>() == 0x02);
+static_assert(clockPrescaler<4'000UL>() == 0x03);
+
 struct core_init {
+    constexpr static auto clk_init = flow::action("ClkInit"_sc, []() {
+        cli();
+        // Enable clock prescaler pin change.
+        CLKPR = 0x80;
+
+        // With 4 clock cycles, update the clock division factor.
+        CLKPR = clockPrescaler<clock_frequency_kHz>();
+        sei();
+    });
     constexpr static auto timer0_init = flow::action("TimerInit"_sc, []() {
         constexpr auto prescaler = 1024UL;
 
@@ -59,6 +80,6 @@ struct core_init {
 #endif
     });
 
-    constexpr static auto config = cib::config(cib::extend<RuntimeInit>(
-        core_init::timer0_init >> core_init::disable_usart));
+    constexpr static auto config = cib::config(
+        cib::extend<RuntimeInit>(clk_init >> timer0_init >> disable_usart));
 };
